911e2ede35716f7d852c140334cafc7aaf3de501
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-06-26 17:17:00 -0400

    Setting up a 404
    react-router-dom provides a Switch to parse any Router, this is useful to setup a 404 page.
    -We created a new component NoFoundPage
    -We added within our Switch component of react-router-dom without path
    -Switch runs like an switch in a programming language, choosing the exact match, and if its not found we catch our default, in our case the NotFoundPage because don't have a path.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -15,14 +15,17 @@ const EditExpensePage = () => <div>This is from my Edit Page</div>;
 
 const HelpExpensePage = () => <div>This is from my Help Page</div>;
 
+const NotFoundPage = () => <div>404! </div>;
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpExpensePage}/>
-    </div>
+      <Route path="/help" component={HelpExpensePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
