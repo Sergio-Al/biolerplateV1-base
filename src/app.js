3f5c1108cea278ea99286ca90dfb282e3d454190
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-06-26 18:18:48 -0400

    Organizing our routes
    We created separated components to our files.
    - We take care about importing: React for jsx and react-routes-dom for links
    - Import all isolated components to our AppRouter.js
    - Run

--- src/app.js
+++ src/app.js
@@ -1,57 +1,8 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import AppRouter from "./routes/AppRouter";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-);
-
-const AddExpensePage = () => <div>This is from my add expense compenent</div>;
-
-const EditExpensePage = () => <div>This is from my Edit Page</div>;
-
-const HelpExpensePage = () => <div>This is from my Help Page</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Home
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit Expense
-    </NavLink>
-    <NavLink to="help" activeClassName="is-active">
-      help
-    </NavLink>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpExpensePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter/>, document.getElementById("app"));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const EditExpensePage = () => <div>This is from my Edit Page</div>;
+
+export default EditExpensePage;
