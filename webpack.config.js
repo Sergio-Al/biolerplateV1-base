0a48fda2cd07332e503b921c6e4a71e834787f88
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-07-16 22:30:55 -0400

    Setting up Redux
    - We installed redux to state management data throughout our react app.
    - We created a new playground file redux-101.js.
    - We configured in webpack as an entry point.
    - We imported createState from redux and create a store constant to manager our state store. Our state store have a callback function as a parameter, our parameter as function have a parameter data by default 'state' and 'state' have a default params as object with a count:0 as values, this is because whether our data is null or undefined always have the default to return.
    - We printed our state as store.getState().

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   // where it should start
-  entry: "./src/app.js",
+  entry: "./src/playground/redux-101.js",
   // where it should end
   output: {
     path: path.join(__dirname, "public"),
