b6394fd7feebe5d18e40b0300eb61252f8b74c21
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-13 19:43:51 -0400

    ES6 Array destructuring
    This lesson enhanced again my knowledge about destructuring:
    - we can rename our value in a new variable.
    - We can establish a default value in case that value doesn't exists

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+//
+// Object destructuring
+
 // const person = {
 //   name: "Sergio",
 //   age: 23,
@@ -17,12 +21,43 @@
 //   console.log(`its ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {},
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {},
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher; // Structure: { [element]: [newName Opt?] = [DefaultValue Opt?] } = [Object].
+// const { name: publisherName = "Self-Published" } = book.publisher; // Structure: { [element]: [newName Opt?] = [DefaultValue Opt?] } = [Object].
 
-console.log(publisherName); // Default: Self-Published, other: Penguin.
+// console.log(publisherName); // Default: Self-Published, other: Penguin.
+
+
+
+
+
+
+
+//
+//
+// Array destructuring
+
+// const address = [
+//   "1299 S Juniper Street",
+//   "Philadelphia",
+//   "Pennsylvania",
+//   "192394",
+// ];
+
+// const address2 = [];
+
+// const [, , city, state] = address;
+// const [, , state2 = "New York"] = address2; // matching by position
+
+// console.log(`You are in ${city} ${state}`);
+// console.log(`You are in ${state2}`);
+
+const item = ["Coffee (iced)", "$3.00", "$3.50", "$3.75"];
+
+const [itemName = "Nothing", , mediumPrice = "$0.00"] = item;
+
+console.log(`A medium ${itemName} cost ${mediumPrice} `);
