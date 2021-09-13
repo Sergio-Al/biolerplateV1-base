86adb91c5eea95748c7b64e7a53503533e4fba0a
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-13 17:35:11 -0400

    Subscribing and Dynamic Actions
    -Subscribe: executes something when a state changes.
    -Unsubscribe: stop executing subscribe process. (you can call unsubscribe as function ()).
    -You can assign custom values and registering in our store main function, mutating the state.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,26 +3,37 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
     case "RESET":
       return {
         count: 0,
+      };
+    case "SET":
+      return {
+        count: action.count, // use action to use the action.count argument value.
       };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
   type: "INCREMENT",
+  incrementBy: 5,
 });
 
 store.dispatch({
@@ -37,4 +48,12 @@ store.dispatch({
   type: "DECREMENT",
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10,
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101, // We can put custom values to our count and redux know about them
+});
