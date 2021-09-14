1902ac3d80e56ff712e474ec04e712fc439c24fe
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-14 12:29:55 -0400

    Sorting Redux data
    - We configured our data to sort by date and amount.
    - we use the sort function with comparefunction because we are working with objects.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -121,17 +121,25 @@ const filtersReducer = (state = filtersReducerDefaultState, actions) => {
 
 // Get visible Expenses
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses.filter((expense) => {
-    const startDateMatch =
-      typeof startDate !== "number" || expense.createdAt >= startDate;
-    const endDateMatch =
-      typeof endDate !== "number" || expense.createdAt <= endDate;
-    const textMatch =
-      typeof text !== "string" ||
-      expense.description.toLowerCase().includes(text.toLowerCase());
+  return expenses
+    .filter((expense) => {
+      const startDateMatch =
+        typeof startDate !== "number" || expense.createdAt >= startDate;
+      const endDateMatch =
+        typeof endDate !== "number" || expense.createdAt <= endDate;
+      const textMatch =
+        typeof text !== "string" ||
+        expense.description.toLowerCase().includes(text.toLowerCase());
 
-    return startDateMatch && endDateMatch && textMatch;
-  });
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "date") {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      } else if (sortBy === "amount") {
+        return a.amount < b.amount ? 1 : -1;
+      }
+    });
 };
 
 // store creation
@@ -158,10 +166,10 @@ const expenseTwo = store.dispatch(
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter("ee"));
+// store.dispatch(setTextFilter("ee"));
 // store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount()); // amount
+store.dispatch(sortByAmount()); // amount
 // store.dispatch(sortByDate()); // date
 
 //store.dispatch(setStartDate(125)); // startDate 125
