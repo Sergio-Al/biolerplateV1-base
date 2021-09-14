b915fa0347c65a70809509b02c7fc45e6023f850
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-14 00:33:15 -0400

    Spreading Objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -23,7 +23,16 @@ const removeExpense = ({ id } = {}) => ({
   id,
 });
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  id,
+  updates,
+});
 // SET_TEXT_FILTER
+const setTextFilter = (text = "") => ({
+  type: "SET_TEXT_FILTER",
+  text,
+});
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -39,6 +48,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       return [...state, action.expense];
     case "REMOVE_EXPENSE":
       return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -54,6 +74,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, actions) => {
   switch (actions.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text: actions.text,
+      };
     default:
       return state;
   }
@@ -79,8 +104,9 @@ const expenseTwo = store.dispatch(
 );
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
-
-console.log(expenseOne);
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter());
 
 const demoState = {
   expenses: [
@@ -99,3 +125,10 @@ const demoState = {
     endDate: undefined,
   },
 };
+
+// const user = {
+//   name: "Jen",
+//   age: 24,
+// };
+
+// console.log({ ...user, location: "Philadelphia", age: 27 });
