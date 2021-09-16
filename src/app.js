450a91c87e41be4d22ee0497f18fce8c3bfe81a4
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-15 22:49:33 -0400

    Connecting store and Component with React-Redux
    - We use Provider to provide store to all components.
    - We use Provide as our main component in ReactDOM.render() as first argument.
    - Now our data state is reactive and updates on all components.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // install -> import -> use
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; // Provide store to all components
 import AppRouter from "./routes/AppRouter";
 import ConfigureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
@@ -18,9 +19,19 @@ store.dispatch(addExpense({ description: "Gas Bill" }));
 //store.dispatch(setTextFilter("bill"));
 store.dispatch(setTextFilter("water"));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter("Gas"));
+}, 3000);
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
