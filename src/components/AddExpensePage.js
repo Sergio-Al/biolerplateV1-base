c2a2dd83a1a1e84c1440b154f19b97bb41b887d8
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-16 18:17:22 -0400

    Creating Expense Add/Edit form
    -We created our form to add and edit our expenses.
    - We added this form to add expense page.
    - Used Regular expressions to make our custom type (numbers with two decimals).
    - Now we have a form to add and edit new expenses.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import ExpenseForm from "./expenseForm";
 
-const AddExpensePage = () => <div>This is from my add expense compenent</div>;
+const AddExpensePage = () => (
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
+);
 
 export default AddExpensePage;
