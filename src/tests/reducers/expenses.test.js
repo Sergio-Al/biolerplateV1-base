d4dff415083a7848ec3f404ee986b9e6828ca72b
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-20 16:02:25 -0400

    Testing expenses reducer
    - We test our expenses reducer
    - We must remember that toEqual is more accurate when test arrays and objects.
    - We use the spread operator.
    - All is working correctly

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,65 @@
+import expensesReducer from "../../reducers/expenses";
+import expenses from "../fixtures/expenses";
+
+test("should set default state", () => {
+  const state = expensesReducer(undefined, { type: "@INIT" });
+  expect(state).toEqual([]);
+});
+
+test("should remove expense by id", () => {
+  const action = {
+    type: "REMOVE_EXPENSE",
+    id: expenses[1].id,
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test("should not remove expense if id not found", () => {
+  const action = {
+    type: "REMOVE_EXPENSE",
+    id: "-1",
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test("should add an expense", () => {
+  const action = {
+    type: "ADD_EXPENSE",
+    expense: {
+      id: "4",
+      description: "Pc",
+      note: "",
+      amount: 34,
+      createdAt: 0,
+    },
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, action.expense]);
+});
+
+test("should edit an expense", () => {
+  const updateDescription = "Credit";
+  const action = {
+    type: "EDIT_EXPENSE",
+    id: "3",
+    updates: {
+      description: updateDescription,
+    },
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[2].description).toBe(updateDescription);
+});
+
+test("should not edit expense if expense not found", () => {
+  const action = {
+    type: "EDIT_EXPENSE",
+    id: "-34",
+    updates: {
+      description: "fail value",
+    },
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
