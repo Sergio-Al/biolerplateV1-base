464b18243587c518165baaafbd86c1aae1ee5a6b
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-21 13:26:13 -0400

    Testing user interaction
    - We managed the user interaction of ExpenseForm in our test file.
    - We used enzyme function like find wich works as getqueryselector in plain javascript.
    - We can access individual child components with index 'at([index of child])'.
    - We can access to a state of our component with .state([individual state prop]) if nothing is passes as argument all state will be returned.
    - We can establish more than a 1 snapshot.
    - With all of this everything user interaction worked correctly.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,49 @@ test("should render expense form with data correctly", () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[2]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("should render error for ivalid sumbission", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find("form").simulate("submit", {
+    preventDefault: () => {}, // this is for simulate an prevent default event
+  });
+  expect(wrapper.state("error").length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot(); // We can have more than 1 snapshot to watch states before/after
+});
+
+test("should set description on input change", () => {
+  const value = "New description";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("input").at(0).simulate("change", {
+    target: { value },
+  });
+  expect(wrapper.state("description")).toBe(value);
+});
+
+test("should set note on textarea change", () => {
+  const value = "this is my textarea message";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("textarea").simulate("change", {
+    target: { value }, // simulating the e event wich uses a target then we assign our value variable.
+  });
+  expect(wrapper.state("note")).toBe(value);
+});
+
+test("should set amount if valid input", () => {
+  const value = "23.34";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("input").at(1).simulate("change", {
+    target: { value },
+  });
+  expect(wrapper.state("amount")).toBe(value);
+});
+
+test("should not set amount if invalid input", () => {
+  const value = "234.343";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("input").at(1).simulate("change", {
+    target: { value },
+  });
+  expect(wrapper.state("amount")).toBe("");
+});
