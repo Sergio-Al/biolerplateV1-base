6274976483331907e77e1b5c789fe026085d1545
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-22 15:35:27 -0400

    Production Webpack
    We configured our webpack file to allow production build.
    - As we using webpack 5 we need to change our script in package.json and add build:dev, build:prod
    - We need to change our source map for production IMPORTANT, this means that our bundle file will be reduced in order to build with a determined source map.
    - We need 'source-map' for production and 'eval-cheap-module-source-map' for development this last in order to use our dev tools and because development build size will be more big than production.
    - We modified our ExpenseForm component in order to show our calendar correclty (i don't know how to do thid through jest).

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 const path = require("path");
 // entry  --> output
 
-module.exports = {
-  mode: "development",
-  // where it should start
-  entry: "./src/app.js", // This line has been modified in the redux section of the react course
-  // where it should end
-  output: {
-    path: path.join(__dirname, "public"),
-    // filename you can put anything name that you like
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  console.log("env", env);
+  return {
+    mode: "development",
+    // where it should start
+    entry: "./src/app.js", // This line has been modified in the redux section of the react course
+    // where it should end
+    output: {
+      path: path.join(__dirname, "public"),
+      // filename you can put anything name that you like
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
