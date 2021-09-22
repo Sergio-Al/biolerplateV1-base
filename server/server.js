3d10b1ec48989f0a8d0fe323d92bd4280c42e774
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-22 18:02:29 -0400

    Setup Production build and server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public"); // '__dirname' is the current directory and the other arguments are files that we want to attach to this current folder.
+const port = process.env.PORT || 3000; // using some javascript or (||) feature which if the first is false return the second one.
+// In order to deploy this to heroku, REMEMBER heroku provides an env file, that because we are set a process.env.PORT value to serve our app
 
 app.use(express.static(publicPath));
 
@@ -9,6 +11,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 }); // configure custom process when exists a get request. In this case we configure something when cannot get a react route with * means all directions
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is up");
 });
