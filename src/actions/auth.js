c92adfec6b9bf046f7a2cf13be7cd1685139861f
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-09-30 16:21:10 -0400

    Logging Out
    We managed our logging out function from firebase.
    - We connected our header js with redux through connect()().
    - We managed an action in our header js  throught dispatch.
    - we created a new action to logout using firebase library.
    - We created test cases of our startlogout button click ensuring that is called (we used spies to make a fake call function).
    - We created spies to our Login button too, in order to make test.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,15 @@
 import { googleAuthProvider } from "../firebase/firebase";
-import { signInWithPopup, getAuth } from "firebase/auth";
+import { signInWithPopup, getAuth, signOut } from "firebase/auth";
 
 export const startLogin = () => {
   return () => {
     console.log("Accessing to startLoginAction");
     signInWithPopup(getAuth(), googleAuthProvider);
+  };
+};
+
+export const startLogout = () => {
+  return () => {
+    return signOut(getAuth());
   };
 };
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { shallow } from "enzyme";
 import ReactShallowRenderer from "react-test-renderer/shallow"; // this will not render child components (use DOM renderer for that)
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
 
 // react-test-renderer //render components in javascript code
 test("should render header correctly", () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => {}} />);
   expect(wrapper).toMatchSnapshot();
 
   //   expect(wrapper.find("h1").text()).toBe("Expensify");
   //   const renderer = new ReactShallowRenderer();
   //   renderer.render(<Header />);
   //   expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+test("should call startLogout on button click", () => {
+  let startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find("button").simulate('click')
+  expect(startLogout).toHaveBeenCalled();
 });
