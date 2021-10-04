2b09529347be80307b04edb1a45c5f150f92c6d1
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-10-04 19:23:11 -0400

    Styling Expenses List: Part I
    Now we focus on the list of expenses, IMPORTANT: WE MAKE HEADER LIST RESPONSIBLE
    - We need to do some restructuring to have the things clear.
    - We need to create style documents.

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,17 +4,13 @@ import moment from "moment";
 import numeral from "numeral";
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <h3>
-      Description: {description}
-      <Link to={`/edit/${id}`}> Edit</Link>
-    </h3>
-    <p>
-      {numeral(amount / 100).format("$0,0.00")}
-      -
-      {moment(createdAt).format("MMMM Do, YYYY")}
-    </p>
-  </div>
+  <Link className="list-item" to={`/edit/${id}`}>
+    <div>
+      <h3>{description}</h3>
+      <span>{moment(createdAt).format("MMMM Do, YYYY")}</span>
+    </div>
+    <h3>{numeral(amount / 100).format("$0,0.00")}</h3>
+  </Link>
 );
 
 export default ExpenseListItem;
